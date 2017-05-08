bb21086fa37aaf0eda9fdb6218b999a6e3a9e979
Alfred Paul <alfred.paul@mindtree.com>
2017-05-08 19:03:33 +0530

    Changes

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,44 +1,37 @@
 var empApp = angular.module('empApp', []);
-empApp.controller('emp-app-controller', ['$scope', '$http', function($scope, $http) {    
-    
+empApp.controller('emp-app-controller', ['$scope', '$http', function($scope, $http) {
   var updateView=function(){
-         $http.get('/employee').success(function(response) {
-         $scope.employees = response; 
-         $scope.employee="";
-        });
+    $http.get('/employee').success(function(response) {
+      $scope.employees = response;
+      $scope.employee="";
+    });
   };
 
   updateView();
-  
-    $scope.addEmployee = function() {
-      $http.post('/employee', $scope.employee).success(function(response) {
+
+  $scope.addEmployee = function() {
+    $http.post('/employee', $scope.employee).success(function(response) {
+      updateView();
+    });
+  };
+
+  $scope.removeEmployee = function(id) {
+    $http.delete('/employee/' + id).success(function(response) {
         updateView();
-      });
-    };
-    
-    $scope.removeEmployee = function(id) {
-      $http.delete('/employee/' + id).success(function(response) {
-          updateView();
-      });
-    };
-    
-    $scope.editEmployee = function(id) {
+    });
+  };
+
+  $scope.editEmployee = function(id) {
       console.log(id);
       $http.get('/employee/' + id).success(function(response) {
       $scope.employee = response;
-     });
-   };
-    
-    $scope.updateEmployee = function() {
-     console.log($scope.employee._id);
-     $http.put('/employee/' + $scope.employee._id, $scope.employee).success(function(response) {
-       updateView();
-     });
-    };
-    
+    });
+  };
 
+  $scope.updateEmployee = function() {
+    console.log($scope.employee._id);
+    $http.put('/employee/' + $scope.employee._id, $scope.employee).success(function(response) {
+     updateView();
+    });
+  };
 }]);
-
-
-
-
